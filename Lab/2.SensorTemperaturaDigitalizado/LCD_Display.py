ccd530f52b5af70e324a4f868d784fc436b505c5
Luis Serrano <luisserranoch96@gmail.com>
2018-04-10 10:33:05 -0700

    Update LCD_Display.py

--- Lab/2.SensorTemperaturaDigitalizado/LCD_Display.py
+++ Lab/2.SensorTemperaturaDigitalizado/LCD_Display.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 from RPLCD import CharLCD
 
-#Establishes the LCD parameters and the pins that are going to be used, in this case 37, 35, 33, 31, 29, 23
+#Define GPIO pins for the LCD
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33,31,29,23],numbering_mode=GPIO.BOARD)
 GPIO.setwarnings(False)
 

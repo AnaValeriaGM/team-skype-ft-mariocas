228b5b2be8be5390c49b83e08d95e05bdf69d60e
Luis Serrano <luisserranoch96@gmail.com>
2018-04-10 11:32:59 -0700

    Update LCD_Display.py

--- Lab/2.SensorTemperaturaDigitalizado/LCD_Display.py
+++ Lab/2.SensorTemperaturaDigitalizado/LCD_Display.py
@@ -5,6 +5,8 @@ from RPLCD import CharLCD
 
 #Define GPIO pins to LCD mapping.
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33,31,29,23],numbering_mode=GPIO.BOARD)
+
+#Set warnings of the default input pins to False.
 GPIO.setwarnings(False)
 
 #Initialization of the LCD

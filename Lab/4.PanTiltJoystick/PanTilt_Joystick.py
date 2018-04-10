94b545c682ed0aab59ac54ae12998f60c7dc3958
Luis Serrano <luisserranoch96@gmail.com>
2018-04-10 10:58:17 -0700

    Update PanTilt_Joystick.py

--- Lab/4.PanTiltJoystick/PanTilt_Joystick.py
+++ Lab/4.PanTiltJoystick/PanTilt_Joystick.py
@@ -9,31 +9,31 @@ GPIO.setmode(GPIO.BOARD)
 Motor1 = 22
 Motor2 = 21
 
-#Setups every channel used as Input or Output
+#Setup every channel used as Input or Output
 GPIO.setup(Motor1,GPIO.OUT)
 GPIO.setup(Motor2,GPIO.OUT)
 
-#Frequency used by the Pulse Width Modulation (PWM) of both motors.
+#Frequency used in the Pulse Width Modulation (PWM) of both motors.
 fq = 50
 
-#Creates a PWM instance for Motors 1 and 2.
+#Create a PWM instance for Motors 1 and 2.
 M1 = GPIO.PWM(22, fq)
 M2 = GPIO.PWM(21, fq)
 
 #Create a SMBus instance.
 bus = smbus.SMBus(1)
-#Address that our i2c device will read from.
+#Address that the i2c device will read from.
 DEVICE_ADDRESS = 0x48
 print('starting...\n')
 
-#Method that scales our input into a range of variables (from 0.0 to 100.0) for PWM.
+#Create a method that scales the input into PWM values (from 0.0 to 100.0).
 def Read(Input):
     x = float(Input)
     x = x/25.6
     print('\nInput = ', Input,'	PWM = ',x) 
     return x
 
-#Loop that reads from our i2c device, which the joystick is connected to, and using our method 'Read()' sends a scaled signal to our motors.
+#Create a loop that reads from the i2c device, interprets and sends a scaled signal to the motors.
 while True:
     #Only the axis x (address 0x00) and y (address 0x01) where used from the joystick.
     x = bus.read_byte_data(DEVICE_ADDRESS, 0x00)
